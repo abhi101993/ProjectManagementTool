0997706e4328b2db602735e01f754cefa1ab5263
Abhishek <abhi101993@gmail.com>
2021-06-15 00:05:33 +0530

    Create Project Repository

--- Java/management_tool/src/main/java/com/management/tool/domain/Project.java
+++ Java/management_tool/src/main/java/com/management/tool/domain/Project.java
@@ -1,5 +1,128 @@
 package com.management.tool.domain;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Project")
 public class Project {
+
+@Id
+@Column(name="id")
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long id;
+
+@Column(name="project_name")
+private String projectName;
+
+@Column(name="project_id")
+private String projectIdentifier;
+
+@Column(name="description")
+private String description;
+
+@Column(name="start_date")
+private Date startDate;
+
+
+@Column(name="end_date")
+private Date endDate;
+
+@Column(name="created_at")
+private Date createdAt;
+
+@Column(name="updated_at")
+private Date updatedAt;
+
+Project()
+{
+	
+}
+
+@PrePersist
+protected void onCreate()
+{
+	this.createdAt = new Date();
+}
+
+@PreUpdate
+protected void onUpdate()
+{
+	this.createdAt = new Date();
+}
+
+public Long getId() {
+	return id;
+}
+
+public void setId(Long id) {
+	this.id = id;
+}
+
+public String getProjectName() {
+	return projectName;
+}
+
+public void setProjectName(String projectName) {
+	this.projectName = projectName;
+}
+
+public String getProjectIdentifier() {
+	return projectIdentifier;
+}
+
+public void setProjectIdentifier(String projectIdentifier) {
+	this.projectIdentifier = projectIdentifier;
+}
+
+public String getDescription() {
+	return description;
+}
+
+public void setDescription(String description) {
+	this.description = description;
+}
+
+public Date getStartDate() {
+	return startDate;
+}
+
+public void setStartDate(Date startDate) {
+	this.startDate = startDate;
+}
+
+public Date getEndDate() {
+	return endDate;
+}
+
+public void setEndDate(Date endDate) {
+	this.endDate = endDate;
+}
+
+public Date getCreatedAt() {
+	return createdAt;
+}
+
+public void setCreatedAt(Date createdAt) {
+	this.createdAt = createdAt;
+}
+
+public Date getUpdatedAt() {
+	return updatedAt;
+}
+
+public void setUpdatedAt(Date updatedAt) {
+	this.updatedAt = updatedAt;
+}
+
+
 
 }
