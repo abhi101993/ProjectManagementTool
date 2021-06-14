8dec01c3debd0a99f124650fde90723d49bebe06
Abhishek <abhi101993@gmail.com>
2021-06-14 22:37:03 +0530

    first commit

---
+++ Java/management_tool/src/test/java/com/management/tool/ManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
